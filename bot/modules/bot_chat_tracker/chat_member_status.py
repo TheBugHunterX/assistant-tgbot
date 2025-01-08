49ac08b634c49e217973cc56144dd2c3ba807022
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-01-08 15:14:17 +0600

    Restored chat_member_status & track_bot_chat file
    - fixed(user wasn't registering in database)

--- bot/modules/bot_chat_tracker/chat_member_status.py
+++ bot/modules/bot_chat_tracker/chat_member_status.py
@@ -6,9 +6,26 @@ async def _chat_member_status(chat_member_update: ChatMemberUpdated):
     if not status:
         return
     
-    user_exist = None
+    user_exist, cause = None, None
     old_status, new_status = status
 
     user_exist = False if new_status in [ChatMember.LEFT, ChatMember.BANNED] else True
+    
+    if old_status in [ChatMember.LEFT, ChatMember.BANNED] and new_status in [ChatMember.MEMBER, ChatMember.ADMINISTRATOR]:
+        cause = "JOINED"
+    elif old_status not in [ChatMember.RESTRICTED, ChatMember.LEFT, ChatMember.BANNED] and old_status and new_status == ChatMember.LEFT:
+        cause ="LEFT"
+    elif old_status != ChatMember.BANNED and new_status == ChatMember.BANNED:
+        cause ="BANNED"
+    elif old_status == ChatMember.BANNED and new_status != ChatMember.BANNED:
+        cause = "UNBANNED"
+    elif old_status != ChatMember.RESTRICTED and new_status == ChatMember.RESTRICTED:
+        cause = "RESTRICTED"
+    elif old_status == ChatMember.RESTRICTED and new_status != ChatMember.RESTRICTED:
+        cause = "UNRESTRICTED"
+    elif old_status != ChatMember.ADMINISTRATOR and new_status == ChatMember.ADMINISTRATOR:
+        cause = "PROMOTED"
+    elif old_status == ChatMember.ADMINISTRATOR and new_status != ChatMember.ADMINISTRATOR:
+        cause = "DEMOTED"
 
-    return user_exist
+    return user_exist, cause
